6fc7b408c26724bf5a70d3d50750183969b7a9fb
Thet Hmuu Eain Soe <richardfromsv@gmail.com>
2020-08-18 09:06:13 +0630

    Authentication and Initial Layout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to posts_path
+    else
+      flash.now[:danger] = 'Login Failed!'
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = "Logged Out!"
+    redirect_to new_session_path
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :name,  presence: true, length: { maximum: 30 }
-    validates :email, presence: true, length: { maximum: 255 }
-    validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+    validates :password, presence: true, length: { minimum: 6 }
 end
